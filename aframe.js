c7fbe0cbedbe9986109b5d280db81565d3409840
Philippe Coval <p.coval@samsung.com>
2019-07-09 08:09:27 +0200

    aframe: Add component logic
    Change-Id: Ia961419bd6867449866c195eade7b75026c1d281
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ aframe.js
@@ -0,0 +1,33 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/#
+ */
+AFRAME.registerComponent('level', {
+  schema: {
+    level: { type: 'number', default: 0},
+  },
+  init: function() {},
+
+  update: function(old) {
+    var properties = this.data;
+    for (var property of Object.keys(properties)) {
+      var rotation = [ 0, 0, 0];
+      switch(property) {
+      case "level":
+        this.level = properties[property] / 100;
+        break;
+      }
+      var el = document.getElementById(property);
+      if (!el || !el.object3D)
+        throw "Null: " + property;
+      el.object3D.scale.set(this.level, this.level, this.level);
+    }
+  }
+});
